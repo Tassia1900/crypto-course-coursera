8f0e199a6b863be0a36cb713ea17474bf4fec0f7
Vïctor Mayoral Vilches <v.mayoralv@gmail.com>
2012-06-29 12:26:37 +0200

    binary_xor completed, as well as switching between ASCII HEX <--> BINARY

--- week1/key-xor-example.py
+++ week1/key-xor-example.py
@@ -3,10 +3,22 @@ import operator, binascii
 
 c=bin(int('6c73d5240a948c86981bc294814d', 16))
 m=bin(int(binascii.hexlify('attack at dawn'), 16))
-k = [0, 'b']
-for i in range(2,len(m)):
-    k.append(int(m[i]) ^ int(c[i]))
+newm = bin(int(binascii.hexlify('attack at dusk'),16))
 
-print m
-print c
-print k
+
+def binary_xor(c,m):
+    k = '0b'
+    if not(len(m) == len(c)):
+        raise Exception('Longitudes no coinciden')
+    for i in range(2,len(m)):
+        k+=str(int(m[i]) ^ int(c[i]))
+    #print k
+    return k
+
+k = binary_xor(c,m)
+nencryption = binary_xor(k,newm)
+# el resultado final en hexadecimal:
+print hex(int(nencryption,2))[2:-1]
+
+# el resultado es:
+# 0b000110100000111101000010100010101101001111111111010110011100111111011000011101110100110111101011111011000100011
