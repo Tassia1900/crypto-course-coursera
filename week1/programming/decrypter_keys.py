81266965133e8fa2b9169aa400e0514194770ad3
user <user@cr-48-ubuntu.(none)>
2012-07-02 22:52:09 +0200

    backtracking not working

--- week1/programming/decrypter_keys.py
+++ week1/programming/decrypter_keys.py
@@ -105,7 +105,7 @@ def backtracking(i, key, lista, lista_sol):
 def backtracking_xor_hex(i, key, lista, lista_sol):
     if(i == (len(lista) -1)):              
         for j in range(len(lista[i])):
-            c_key = copy.deepcopy(key)
+            c_key = copy.copy(key)
             c_key.append(lista[i][j])
             lista_sol.append(c_key)
             key_candidate = ''.join([c for c in c_key])
@@ -113,13 +113,13 @@ def backtracking_xor_hex(i, key, lista, lista_sol):
             print message
     else:                                 
         if(len(lista[i]) == 0):
-            c_key = copy.deepcopy(key)
+            c_key = copy.copy(key)
             c_key.append('00')
             print '     key candidate for now: '+str(c_key)
             backtracking(i+1,c_key, lista, lista_sol)
         else:
             for j in range(len(lista[i])):
-                c_key = copy.deepcopy(key)
+                c_key = copy.copy(key)
                 c_key.append(lista[i][j])
                 print '     key candidate for now: '+str(c_key)
                 backtracking(i+1,c_key,lista, lista_sol)
